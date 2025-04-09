5154406d48f20f8094caf8958e6e2a5656a6146c
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-04-09 15:28:04 +0100

    create logic to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+// randomly pick rock, paper, or scissors (return as a string)
+function getComputerChoice() {
+  let randomInt = Math.floor(Math.random() * 3); // generate a random integer from 0 to 2
+  let choice;
+  if (randomInt === 0) {
+    choice = "rock";
+  } else if (randomInt === 1) {
+    choice = "paper";
+  } else if (randomInt === 2) {
+    choice = "scissors"
+  }
+  return choice;
+}
+
+// console.log(getComputerChoice());
